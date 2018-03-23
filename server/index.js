9a07d7da323ba6b85e39d84e1f5ec5ca842b0289
Jackson Christoffersen <jacksontchristo@gmail.com>
2018-03-23 15:45:34 -0700

    initialize react app with webpack

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
 const Koa = require('koa');
+const serve = require('koa-static');
+const path = require('path');
 
 const app = new Koa();
 
-app.use(async (ctx) => {
-  ctx.body = 'Hello World';
-});
+app.use(serve(path.join(__dirname, '../client/dist')));
 
 app.listen(1994);
