1c2d991fa92bcc1d28bab771a83f412fabebfd3d
Jackson Christoffersen <jacksontchristo@gmail.com>
2018-06-14 21:09:57 -0700

    add script to delete old workers on build

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,13 @@
 const path = require('path');
 
+const fs = require('fs');
+const WebpackOnBuildPlugin = require('on-build-webpack');
+
+const buildDir = path.join(__dirname, 'client/dist/compiled');
+
+/* eslint no-new: 0 */
+
+
 module.exports = {
   entry: path.join(__dirname, 'client/src/index.jsx'),
   output: {
@@ -30,6 +38,28 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    // remove old worker files
+    new WebpackOnBuildPlugin((stats) => {
+      const newlyCreatedAssets = stats.compilation.assets;
+
+      const unlinked = [];
+      fs.readdir(buildDir, (err, files) => {
+        files.forEach((file) => {
+          if (!newlyCreatedAssets[file]) {
+            fs.unlink(path.resolve(`${buildDir}/${file}`), (error) => {
+              if (error) throw error;
+            });
+            unlinked.push(file);
+          }
+        });
+
+        if (unlinked.length > 0) {
+          console.log('Removed old assets: ', unlinked);
+        }
+      });
+    }),
+  ],
   mode: 'development',
   resolve: { extensions: ['.js', '.jsx'] },
 };
